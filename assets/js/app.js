fc02dec615a1d68ef26d90b81bd22c99c6caf197
LucFII <LSFIIChi@Gmail.com>
2017-06-06 17:32:57 -0500

    Beginning to add html and js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,64 @@
 
 // ==============  VARIABLES  ===============
 
+var questions = [
+  {
+    question: "How tall is the John Hancock Center (Antenna Included) ?",
+    answer: ["457 meters", "503 meters", "247 meters", "463 meters"],
+    correctAnswer: "457 meters"
+  },
+   {
+    question: "How tall is the John Hancock Center (Antenna Excluded) ?",
+    answer: ["198 meters", "332 meters", "343 meters", "247 meters"],
+    correctAnswer: "343 meters"        
+  },
+   {
+    question: "How tall is the John Hancock Center (Antenna Excluded) ?",
+    answer: ["198 meters", "332 meters", "343 meters", "247 meters"],
+    correctAnswer: "343 meters"        
+  },
+     {
+    question: "How tall is the John Hancock Center (Antenna Excluded) ?",
+    answer: ["198 meters", "332 meters", "343 meters", "247 meters"],
+    correctAnswer: "343 meters"        
+  },
+     {
+    question: "How tall is the John Hancock Center (Antenna Excluded) ?",
+    answer: ["198 meters", "332 meters", "343 meters", "247 meters"],
+    correctAnswer: "343 meters"        
+  },
+     {
+    question: "How tall is the John Hancock Center (Antenna Excluded) ?",
+    answer: ["198 meters", "332 meters", "343 meters", "247 meters"],
+    correctAnswer: "343 meters"        
+  },
+];
 
 
 // ==============  EVENTS  ==================
 
-
-
+/* initizlize */
+        /* button click */
+        $('#start').on('click', function() {
+          startGame();
+        })
 
 // ==============  FUNCTIONS  ===============
+
+function startGame() {
+	var j = 0;
+	var questionString = '';
+        for(var i=0; i < questions.length; i++) {
+          j++;
+
+          var answerString = '';
+          
+          questionString = '<div>' + j + '. ' + questions[i].question + '</div>';
+
+          for(var a=0; a < questions[i].answer.length; a++) {
+            answerString += '<input class="answer" type="radio">' + questions[i].answer[a] + '</input>';
+          }
+
+          $('#questions').append('<div class="question">' + questionString + answerString + '</div>');
+    }
+}
